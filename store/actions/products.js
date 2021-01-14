b5dbd7e6f9b36771189ca73d09996268ec263719
Frandosis <48022167+Frandosis@users.noreply.github.com>
2021-01-14 16:00:18 +0100

    Firestore Fetch data

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,40 +1,52 @@
-import Product from '../../models/product';
-import firebase from "../../firebase/index"
-export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+import Product from "../../models/product";
+import { firebase } from "../../firebase";
+
+require("firebase/firestore");
+export const FETCH_PRODUCTS = "FETCH_PRODUCTS";
 
 export const fetchProducts = () => {
-    console.log('j')
+  console.log("j");
 
-    return async dispatch => {
-      // any async code you want!
-      console.log('jjkj')
-//      console.log( await firebase.firestore().collection('users').get())
+  return async (dispatch) => {
+    // any async code you want!
+    console.log("jjkj");
+    let products = [];
+    //first get the collection with the wanted documents.
+    await firebase
+      .firestore()
+      .collection("product")
+      .get()
+      .then((snapshot) => {
+        //console.log(snapshot);
+        //Map all the documents in sorted querysnapshot.
+        snapshot.docs.forEach((doc) => {
+          products.push({ id: doc.id, ...doc.data() });
+        });
+      });
+    /*
+    const fetchData = await fetch(
+      "https://blue-market-1a247-default-rtdb.firebaseio.com/products.json"
+    );
+    const doc = await fetchData.json();
+    //console.log(doc);
+    const objectsArray = [];
+    for (const key in doc) {
+      console.log("wdji");
 
-        const fetchData = await fetch(
-            'https://blue-market-1a247-default-rtdb.firebaseio.com/products.json'
-        );
-        const doc = await fetchData.json();
-        console.log(doc)
-        const objectsArray = [];
-        for (const key in doc) {
-          console.log('wdji')
-
-          objectsArray.push(
-              new Product(
-                key,
-                doc[key].id,
-                doc[key].title,
-                doc[key].imageUrl,
-                doc[key].description,
-                doc[key].price
-
-              )
-
-              );
-        }
-        console.log('wdji')
-        console.log(objectsArray);
-  
-        dispatch({ type: FETCH_PRODUCTS, products: objectsArray });
-    };
+      objectsArray.push(
+        new Product(
+          key,
+          doc[key].id,
+          doc[key].title,
+          doc[key].imageUrl,
+          doc[key].description,
+          doc[key].price
+        )
+      );
+    }
+    */
+    console.log("wdji");
+    console.log(products);
+    dispatch({ type: FETCH_PRODUCTS, products: products });
   };
+};
