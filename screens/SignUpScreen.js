4e5f3d739273bfac583e7095d4702e697dc0ff75
Frandosis <48022167+Frandosis@users.noreply.github.com>
2021-01-18 17:53:55 +0100

    Choosing locally stored profile pics

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -9,6 +9,8 @@ C:/Users/Valde/Documents/LAPTOP-PLFGULU9/Documents/It-elektronik/5. Semester/Sel
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import { IconButton, Title, Text, Avatar, Snackbar } from "react-native-paper";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
 import FormButton from "../components/FormButton";
 import FormInput from "../components/FormInput";
@@ -17,7 +19,23 @@ import Loading from "../components/Loading";
 import { firebase } from "../firebase";
 
 export default function SignupScreen({ navigation }) {
+  const [
+    camPermission,
+    askForCameraPermission,
+    getCameraPermission,
+  ] = Permissions.usePermissions(Permissions.CAMERA, { ask: true });
+
+  const [
+    storagePermission,
+    askForStoragePermission,
+    getStoragePermission,
+  ] = Permissions.usePermissions(Permissions.MEDIA_LIBRARY, { ask: true });
+
   const [credential, setCredential] = useState(null);
+  const [localProfileURL, setLocalProfileURL] = useState("");
+  const [cloudProfileURL, setCloudProfileURL] = useState();
+
+  const [defaultPic, setDefaultPic] = useState(true);
   const [profilePic, setProfilePic] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -30,24 +48,58 @@ export default function SignupScreen({ navigation }) {
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    async function fetchData() {
-      // You can await here
-      await firebase
-        .storage()
-        .ref()
-        .child("default_profile_pic.jpg")
-        .getDownloadURL()
-        .then((url) => {
-          setProfilePic(url);
-        });
-    }
-    fetchData();
+    (async () => {
+      // Fetch cloud based profilePic.
+      if (defaultPic) {
+        await firebase
+          .storage()
+          .ref()
+          .child("images/profilepic.jpg")
+          .getDownloadURL()
+          .then((url) => {
+            setProfilePic(url);
+          });
+      }
+    })();
   });
 
   const onDismissSnackBar = () => setVisible(false);
 
+  const takePictureWithCamera = async () => {
+    console.log("Opening Camera");
+    if (!camPermission || camPermission.status !== "granted") {
+      setVisible(true);
+      setErrorMessage("Permission for accessing phone camera wasn't granted");
+      return;
+    }
+    //Permission granted open camera.
+  };
+
+  const fetchPictureFromGallery = async () => {
+    // Check media storage permissions. Send snackbar error if not given.
+    console.log("Opening Gallery");
+    if (!storagePermission || storagePermission.status !== "granted") {
+      setVisible(true);
+      setErrorMessage("Permission for accessing phone storage wasn't granted");
+      return;
+    }
+    //Permission granted open image-picker.
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setDefaultPic(false);
+      setProfilePic(result.uri);
+    }
+  };
+
   const uploadImage = async () => {
-    const childPath = `users/${firebase.auth().currentUser.uid}/profilepic.jpg`;
+    const childPath = `users/${
+      firebase.auth().currentUser.uid
+    }/profile/profilepic.jpg`;
     console.log(childPath);
 
     const response = await fetch(profilePic);
@@ -111,46 +163,60 @@ export default function SignupScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Avatar.Image size={100} source={{ uri: profilePic }}></Avatar.Image>
-      <Title style={styles.titleText}>Let's get started!</Title>
-      <FormInput
-        labelName="Firstname"
-        value={firstName}
-        autoCapitalize="none"
-        onChangeText={(userFirstName) => setFirstName(userFirstName)}
-      />
-      <FormInput
-        labelName="Lastname"
-        value={lastName}
-        autoCapitalize="none"
-        onChangeText={(userLastName) => setLastName(userLastName)}
-      />
-      <FormInput
-        labelName="Email"
-        value={email}
-        autoCapitalize="none"
-        onChangeText={(userEmail) => setEmail(userEmail)}
-      />
-      <FormInput
-        labelName="Password"
-        value={password}
-        secureTextEntry={true}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-      />
-      <FormButton
-        title="Signup"
-        modeValue="contained"
-        labelStyle={styles.loginButtonLabel}
-        onPress={() => register(firstName, lastName, email, password)}
-      />
-      <IconButton
-        icon="keyboard-backspace"
-        size={30}
-        style={styles.navButton}
-        color="#5b3a70"
-        onPress={() => navigation.goBack()}
-      />
-
+      <View style={styles.imageContainer}>
+        <Avatar.Image size={100} source={{ uri: profilePic }}></Avatar.Image>
+      </View>
+      <View style={styles.linkContainer}>
+        <Text onPress={() => takePictureWithCamera()} style={styles.linkText}>
+          {"Take picture"}
+        </Text>
+        <Text onPress={() => fetchPictureFromGallery()} style={styles.linkText}>
+          {"Get picture from Gallery"}
+        </Text>
+        <Text onPress={() => setDefaultPic(true)} style={styles.linkText}>
+          {"Set default picture"}
+        </Text>
+      </View>
+      <View style={styles.formContainer}>
+        <Title style={styles.titleText}>Let's get started!</Title>
+        <FormInput
+          labelName="Firstname"
+          value={firstName}
+          autoCapitalize="none"
+          onChangeText={(userFirstName) => setFirstName(userFirstName)}
+        />
+        <FormInput
+          labelName="Lastname"
+          value={lastName}
+          autoCapitalize="none"
+          onChangeText={(userLastName) => setLastName(userLastName)}
+        />
+        <FormInput
+          labelName="Email"
+          value={email}
+          autoCapitalize="none"
+          onChangeText={(userEmail) => setEmail(userEmail)}
+        />
+        <FormInput
+          labelName="Password"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={(userPassword) => setPassword(userPassword)}
+        />
+        <FormButton
+          title="Signup"
+          modeValue="contained"
+          labelStyle={styles.loginButtonLabel}
+          onPress={() => register(firstName, lastName, email, password)}
+        />
+        <IconButton
+          icon="keyboard-backspace"
+          size={30}
+          style={styles.navButton}
+          color="#5b3a70"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
       <Snackbar
         visible={visible}
         onDismiss={onDismissSnackBar}
@@ -173,12 +239,31 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  imageContainer: {
+    alignItems: "center",
+  },
+  linkContainer: {
+    justifyContent: "space-around",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  formContainer: {
+    alignItems: "center",
+  },
 
   image: {
     flex: 1,
     height: undefined,
     width: undefined,
   },
+
+  linkText: {
+    fontSize: 14,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+
   titleText: {
     fontSize: 24,
     marginBottom: 10,
