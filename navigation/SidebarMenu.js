2f93afa44aa05b7c355c0b41672aee12303d7c63
Frandosis <48022167+Frandosis@users.noreply.github.com>
2021-01-19 16:16:06 +0100

    Error

--- navigation/SidebarMenu.js
+++ navigation/SidebarMenu.js
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { Component, useState } from "react";
 
 import { StyleSheet, ScrollView, View, Image } from "react-native";
+import { Avatar } from "react-native-paper";
 import SafeAreaView from "react-native-safe-area-view";
 import { DrawerItems, DrawerView } from "react-navigation-drawer";
 
 const logo =
   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png";
+
+const InternalMenu = (props) => {
+  return (
+    <View>
+      <Avatar.Image />
+    </View>
+  );
+};
+
 export default SidebarMenu = (props) => (
   <SafeAreaView
     style={styles.container}
     forceInset={{ top: "always", horizontal: "never" }}
   >
-    <View
-      style={{
-        height: 200,
-        backgroundColor: "black",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image source={{ uri: logo }} style={{ height: 100, width: 100 }} />
+    <View style={styles.imagecontainer}>
+      <Avatar.Image source={{ uri: logo }} size={100} />
     </View>
-    <ScrollView>
+    <ScrollView style={styles.navigationcontainer}>
       <DrawerItems {...props} />
+      <DrawerItems></DrawerItems>
     </ScrollView>
   </SafeAreaView>
 );
@@ -31,4 +35,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  imagecontainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  avatar: {
+    height: 100,
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  navigationcontainer: {},
 });
